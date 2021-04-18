4901abdff0a7136f9aa631c703284a0c5b984a01
inezabonte <inezabonte@gmail.com>
2021-04-18 16:00:02 +0200

    switched to nextjs

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const withPWA = require('next-pwa');
+const prod = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+    pwa: {
+        dest: 'public',
+        disable: prod ? false : true
+    },
+    future: {
+        webpack5: true
+    }
+})
